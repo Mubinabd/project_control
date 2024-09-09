75dd3a7e89f40530064fa2ea9952569fe4f23969
Mubina Bahodirova <bahodirovamubina505@gmail.com>
2024-09-09 15:10:33 +0500

    uptaded migrations

--- migrations/000001_create_table.down.sql
+++ migrations/000001_create_table.down.sql
@@ -1,3 +1,7 @@
+drop table if EXISTS tokens;
+drop table if EXISTS settings;
+drop table if EXISTS users;
+drop type if EXISTS role_type;
 drop table if EXISTS documentation;
 drop table if EXISTS private;
 drop table if EXISTS developers;
